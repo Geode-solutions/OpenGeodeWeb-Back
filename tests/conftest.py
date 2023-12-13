15ad3cfe42dcad747acdf470aae9d102a9e2c90d
SpliiT <spliitdev@gmail.com>
2023-12-13 16:28:59 +0100

    feat(flask): add flask app & config edit pytest config

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,12 @@
 import pytest
+from app import app
+
+
+@pytest.fixture
+def client():
+    app.config["TESTING"] = True
+    app.config["SERVER_NAME"] = "TEST"
+    app.config["DATA_FOLDER"] = "./data/"
+    client = app.test_client()
+    client.headers = {"Content-type": "application/json", "Accept": "application/json"}
+    yield client
