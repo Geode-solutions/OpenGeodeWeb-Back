831d3583067216e2aa77cf0556d65e0837fe3a19
SpliiT <spliitdev@gmail.com>
2025-03-26 11:22:11 +0100

    change routes' names & schemas + rv output.og_tsf3d

--- src/opengeodeweb_back/routes/models/blueprint_models.py
+++ src/opengeodeweb_back/routes/models/blueprint_models.py
@@ -23,13 +23,15 @@ def teardown_request(exception):
 
 schemas = os.path.join(os.path.dirname(__file__), "schemas")
 
-with open(os.path.join(schemas, "mesh_components.json"), "r") as file:
-    mesh_components_json = json.load(file)
+with open(os.path.join(schemas, "vtm_component_indices.json"), "r") as file:
+    vtm_component_indices_json = json.load(file)
 
 
-@routes.route(mesh_components_json["route"], methods=mesh_components_json["methods"])
+@routes.route(
+    vtm_component_indices_json["route"], methods=vtm_component_indices_json["methods"]
+)
 def uuid_to_flat_index():
-    utils_functions.validate_request(flask.request, mesh_components_json)
+    utils_functions.validate_request(flask.request, vtm_component_indices_json)
     vtm_file_path = os.path.join(
         flask.current_app.config["DATA_FOLDER_PATH"], flask.request.json["id"] + ".vtm"
     )
@@ -63,13 +65,13 @@ def extract_model_uuids(geode_object, file_path):
     return uuid_dict
 
 
-with open(os.path.join(schemas, "components_types.json"), "r") as file:
-    components_types_json = json.load(file)
+with open(os.path.join(schemas, "mesh_components.json"), "r") as file:
+    mesh_components_json = json.load(file)
 
 
-@routes.route(components_types_json["route"], methods=components_types_json["methods"])
+@routes.route(mesh_components_json["route"], methods=mesh_components_json["methods"])
 def extract_uuids_endpoint():
-    utils_functions.validate_request(flask.request, components_types_json)
+    utils_functions.validate_request(flask.request, mesh_components_json)
 
     file_path = os.path.join(
         flask.current_app.config["DATA_FOLDER_PATH"], flask.request.json["filename"]
