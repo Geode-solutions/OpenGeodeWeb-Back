0b43168d1ce5776c2767172df02e922898c908bb
JulienChampagnol <julien.champagnol@geode-solutions.com>
2025-05-21 14:48:46 +0200

    fix(python 3.12): compatibility

--- src/opengeodeweb_back/utils_functions.py
+++ src/opengeodeweb_back/utils_functions.py
@@ -8,7 +8,7 @@ import zipfile
 import flask
 from jsonschema import validate
 from jsonschema.exceptions import ValidationError
-import pkg_resources
+import importlib.metadata as metadata
 
 # Local application imports
 
@@ -72,9 +72,11 @@ def versions(list_packages: list):
         list_with_versions.append(
             {
                 "package": package,
-                "version": pkg_resources.get_distribution(package).version,
+                "version": metadata.distribution(package).version
+               
             }
         )
+        print(f"{package} version: {metadata.distribution(package)}", flush=True)
     return list_with_versions
 
 
