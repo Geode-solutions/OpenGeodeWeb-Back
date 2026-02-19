092789a2a11f84737a5e0db14094876f19537f06
MaxNumerique <maximilienlandois@gmail.com>
2026-02-19 14:43:22 +0100

    feat(structuralModel): Recursive test_route_wrong_params

--- src/opengeodeweb_back/test_utils.py
+++ src/opengeodeweb_back/test_utils.py
@@ -10,21 +10,34 @@ JsonData = dict[str, Any]
 
 
 def test_route_wrong_params(
-    client: FlaskClient, route: str, get_full_data: Callable[[], JsonData]
+    client: FlaskClient, route: str, get_full_data: Callable[[], JsonData], path: list = []
 ) -> None:
-    for key, value in get_full_data().items():
+    def get_json():
         json = get_full_data()
-        json.pop(key)
+        target = json
+        for p in path:
+            target = target[p]
+        return json, target
+
+    data = get_json()[1]
+    if isinstance(data, dict):
+        for key, value in data.items():
+            json, target = get_json()
+            target.pop(key)
+            response = client.post(route, json=json)
+            if response.status_code == 400:
+                error_description = response.get_json()["description"]
+                assert "must contain" in error_description
+                assert f"'{key}'" in error_description
+            if isinstance(value, (dict, list)):
+                test_route_wrong_params(client, route, get_full_data, path + [key])
+
+        json, target = get_json()
+        target["dumb_key"] = "dumb_value"
         response = client.post(route, json=json)
         assert response.status_code == 400
         error_description = response.get_json()["description"]
-        assert "data must contain" in error_description
-        assert f"'{key}'" in error_description
-
-    json = get_full_data()
-    json["dumb_key"] = "dumb_value"
-    response = client.post(route, json=json)
-    assert response.status_code == 400
-    error_description = response.get_json()["description"]
-    assert "data must not contain" in error_description
-    assert "'dumb_key'" in error_description
+        assert "must not contain" in error_description
+        assert "'dumb_key'" in error_description
+    elif isinstance(data, list) and data:
+        test_route_wrong_params(client, route, get_full_data, path + [0])
